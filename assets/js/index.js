f6f00f5ffe96af785cfdcd17ff73b375f334a25e
TIFFANY <tiffanyliu2008@yahoo.com>
2023-08-12 11:31:36 -0700

    phase 1 done

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,21 +7,20 @@ let id = 0
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 const generateNewID = () => {
-    let idNum = id
+    let idNum = id;
     id++;
-    return idNum
+    return idNum;
 }
-
+const gameboard = document.getElementById('gameboard');
 const populateBoard = function(board) {
     console.log(board)
-    const gameboard = document.getElementById('gameboard')
     for (let i = 0; i < board.grid.length; i++) {
         for (let j = 0; j < board.grid[i].length; j++) {
             console.log(`i `, board.grid[i][j])
             let newDiv = document.createElement('div');
-            newDiv.id = generateNewID();
-            newDiv.className = 'square'
-            gameboard.appendChild(newDiv)
+            newDiv.id = `${i}, ${j}`;
+            newDiv.className = 'square';
+            gameboard.appendChild(newDiv);
         }
     }
 }
@@ -33,5 +32,18 @@ const populateBoard = function(board) {
     //create new div with idNumber
     //append div to gameboard, give id = generateNewId(), give class .square for css style
 
-// Your code here
+
+//clicking a square
+
+gameboard.addEventListener("click", (e) => {
+    const coor = e.target.id.split(", ");
+    if (board.makeHit(coor[0], coor[1])) {
+        e.target.style.backgroundColor = "green";
+        e.target.innerText = board.grid[coor[0]][coor[1]];
+        // console.log(board.grid[coor[0], coor[1]]);
+    } else {
+        e.target.style.backgroundColor = 'red';
+    }
+
+});
 }
